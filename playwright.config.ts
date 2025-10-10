325c95ebc6b9b3f7be91ae22a6c58447c06d52fa
knifey-moloko <maciej.a.cisowski@gmail.com>
2025-10-11 00:36:14 +0200

    Simple login test with storageState write

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: 'https://onskeskyen.dk',
-    storageState: 'storageState.json',
+    storageState: '.auth/storageState.json',
     headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
@@ -45,7 +45,7 @@ export default defineConfig({
       name: 'chromium',
       use: { 
           ...devices['Desktop Chrome'],
-          storageState: 'storageState.json',
+          storageState: '.auth/storageState.json',
       },
       dependencies: ['login'],
     },
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,24 @@
 import { test, expect } from '@playwright/test';
+import path from 'path';
 
+const storageStateFile = path.join(__dirname, '../playwright/.auth/storageState.json')
 const rootURL = 'https://onskeskyen.dk/'
+const overviewURL = 'https://onskeskyen.dk/overview'
 
 test('login with test user', async ({ page }) => {
+    const username = process.env.WISH_USER || 'no USERNAME value!'
+    const password = process.env.PASSWORD || 'no PASSWORD value!'
     await page.goto(rootURL)
     await page.waitForURL("https://onskeskyen.dk/da")
-    await expect((await page.title()).toString()).toBe("Ønskeskyen");
+
+    await page.getByRole('button', { name: 'Accepter alle' }).click();
+    await page.getByRole('button', { name: 'Log ind' }).click();
+    await page.getByRole('button', { name: 'Fortsæt med e-mail' }).click();
+    await page.getByRole('textbox', { name: 'E-mail' }).fill(username);
+    await page.getByTestId('loginPasswordInput').click();
+    await page.getByTestId('loginPasswordInput').fill(password);
+    await page.getByRole('dialog').getByRole('button', { name: 'Log ind' }).click();
+    await page.waitForURL(overviewURL)
+
+    await page.context().storageState({ path: storageStateFile })
 });
