4b581b00d0a4c01b856d5d2ec66606865d4277ef
knifey-moloko <maciej.a.cisowski@gmail.com>
2025-10-24 17:39:47 +0200

    Add a create wishlist flow test

--- tests/catlist.spec.ts
+++ tests/catlist.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '../fixtures/base';
 
 test('visual comparison for cat wishlist', async ({ overviewPage, page }) => {
     await overviewPage.goto();
-    await overviewPage.clickOnCatlistCard()
+    await overviewPage.clickOnCatlistCard();
     await expect(page).toHaveScreenshot('catlist.png', { maxDiffPixelRatio: 0.02 });
 });
